0f7eadfe14c0694fa204bd0a9d5cdaa5e5e59eb6
tattsun <t.t.mc1192.sf@gmail.com>
2018-12-18 00:03:47 +0900

    add profiler

--- main.go
+++ main.go
@@ -1,22 +1,56 @@
 package main
 
-import "os"
+import (
+	"log"
+	"net/http"
+	"net/http/pprof"
+	"os"
+
+	"github.com/urfave/cli"
+)
 
 func main() {
-	configFile, err := os.Open("./config.yml")
-	if err != nil {
-		panic(err)
+	app := cli.NewApp()
+	app.Name = "xproxy"
+	app.Usage = "yet another pac file killer"
+
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "profiler",
+			Usage: "profiler will be started on port 1081",
+		},
 	}
 
-	config, err := ParseConfig(configFile)
-	if err != nil {
-		panic(err)
+	app.Action = func(c *cli.Context) error {
+		if c.Bool("profiler") {
+			go func() {
+				r := http.NewServeMux()
+				r.HandleFunc("/debug/pprof/", pprof.Index)
+				r.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+				r.HandleFunc("/debug/pprof/profile", pprof.Profile)
+				r.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+				r.HandleFunc("/debug/pprof/trace", pprof.Trace)
+				http.ListenAndServe("127.0.0.1:1081", r)
+			}()
+		}
+
+		configFile, err := os.Open("./config.yml")
+		if err != nil {
+			return err
+		}
+
+		config, err := ParseConfig(configFile)
+		if err != nil {
+			return err
+		}
+
+		server, err := NewServer(config.Host, config.Port, config)
+		if err != nil {
+			return err
+		}
+
+		return server.Start()
 	}
 
-	server, err := NewServer(config.Host, config.Port, config)
-	if err != nil {
-		panic(err)
-	}
-
-	panic(server.Start())
+	log.Fatal(app.Run(os.Args))
 }
